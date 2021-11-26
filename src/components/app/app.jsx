1d32b227ec5f7c67c8293c74cac5c81f7c713650
Andrey Filippov <androsphilippos@gmail.com>
2021-11-26 13:55:55 +0500

    создал компонент App

---
+++ src/components/app/app.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../header/header';
+import Footer from '../footer/footer';
+
+import './app.scss';
+
+function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <main>
+
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
