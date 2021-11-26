1c910c33fa82e52406724ad709ff045112c2c896
Andrey Filippov <androsphilippos@gmail.com>
2021-11-26 13:54:19 +0500

    вывел постоянные данные в const

---
+++ src/const.js
@@ -0,0 +1,28 @@
+const MENU_ROUTES = {
+  'Каталог': 'catalog',
+  'Где купить?': 'where-buy',
+  'О компании': 'about',
+  'Cервис-центры': 'service-centers',
+};
+
+const CATALOG_ROUTES = {
+  'Акустические гитары': 'acoustic-guitars',
+  'Классические гитары': 'classic-guitars',
+  'Электрогитары': 'electric-guitars',
+  'Бас-гитары': 'bass-guitars',
+  'Укулеле': 'ukulele',
+};
+
+const INFO_ROUTES = {
+  'Где купить?': 'where-buy',
+  'Блог': 'blog',
+  'Вопрос-ответ': 'question-answer',
+  'Возврат': 'refund',
+  'Сервис-центры': 'service-centers',
+};
+
+export {
+  MENU_ROUTES,
+  CATALOG_ROUTES,
+  INFO_ROUTES,
+};
