14a87535dd4252ac392bd9a8b8deb13bae88ad4a
Andrey Filippov <androsphilippos@gmail.com>
2021-11-26 14:11:21 +0500

    создал компонент About

---
+++ src/components/footer/about/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './about.scss';
+
+function About() {
+  return (
+    <div className="about">
+      <h4>О нас</h4>
+      <p>Магазин гитар, музыкальных инструментов и гитарная мастерская в Санкт-Петербурге.</p>
+      <p>Все инструменты проверены, отстроены и доведены до идеала!</p>
+    </div>
+  );
+}
+
+export default About;
