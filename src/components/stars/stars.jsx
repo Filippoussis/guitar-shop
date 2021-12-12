cf5e65b051cad9c0943ff3e9d53536d700e61f47
Andrey Filippov <androsphilippos@gmail.com>
2021-12-12 11:04:49 +0500

    создал компонент звездного рейтинга

---
+++ src/components/stars/stars.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {ReactComponent as Star} from '../../images/icon/star.svg';
+
+import './stars.scss';
+
+const STARS = ['1', '2', '3', '4', '5'];
+
+function Stars() {
+
+  const items = STARS.map((item) => <li key={item} className="stars__item"><Star /></li>)
+
+  return (
+    <ul className="stars">{items}</ul>
+  );
+}
+
+export default Stars;
