918663576c4625d564f89b6ba8b0c93cc6e526f9
Andrey Filippov <androsphilippos@gmail.com>
2021-12-12 11:02:56 +0500

    вынес константы в отдельный модуль

--- src/const.js
+++ src/const.js
@@ -21,8 +21,57 @@ const INFO_ROUTES = {
   'Сервис-центры': 'service-centers',
 };
 
+const SortDirection = {
+  ASCENDING: 'ascending',
+  DESCENDING: 'descending',
+};
+
+const SortType = {
+  PRICE: 'price',
+  POPULAR: 'popular',
+};
+
+const GUITAR_TYPE = {
+  'acoustic': {
+    title: 'Aкустические гитары',
+    strings: [6, 7, 12],
+  },
+  'electro': {
+    title: 'Электрогитары',
+    strings: [4, 6, 7],
+  },
+  'ukulele': {
+    title: 'Укулеле',
+    strings: [4],
+  },
+};
+
+const GUITARS_STEP = 9;
+
+const GUITAR_STRINGS = [4, 6, 7, 12];
+
+const AppRoute = {
+  ROOT: '/',
+  CART: '/cart',
+};
+
+const DISCOUNT_CODES = ['GITARAHIT', 'SUPERGITARA', 'GITARA2020'];
+
+const BreadcrumbsList = {
+  CATALOG: ['Главная', 'Каталог'],
+  CART: ['Главная', 'Каталог', 'Оформляем'],
+};
+
 export {
   MENU_ROUTES,
   CATALOG_ROUTES,
   INFO_ROUTES,
+  SortDirection,
+  SortType,
+  GUITAR_TYPE,
+  GUITAR_STRINGS,
+  GUITARS_STEP,
+  AppRoute,
+  DISCOUNT_CODES,
+  BreadcrumbsList,
 };
