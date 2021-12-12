aa17cc3d59e90c6ab1ab4eb4edaac787036c7f62
Andrey Filippov <androsphilippos@gmail.com>
2021-12-12 11:11:56 +0500

    создал компонент сообщение о добавлении товара

---
+++ src/components/products/modal-add-to-cart/modal-add-to-cart.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import {ReactComponent as CloseButton} from '../../../images/icon/close.svg';
+
+import {setBodyScroll} from '../../../utils';
+
+import {addItem, setActiveItem, setSuccess} from '../../../store/slices/cart';
+
+import './modal-add-to-cart.scss';
+
+const GUITAR_TYPE = {
+  'acoustic': 'Aкустическая гитара',
+  'electro': 'Электрогитара',
+  'ukulele': 'Укулеле',
+}
+
+function ModalAddToCart({item}) {
+
+  const {title, price, smallImage, bigImage, article, type, strings} = item;
+
+  const dispatch = useDispatch();
+
+  const handleClickAddButton = () => {
+    dispatch(addItem(item));
+    dispatch(setActiveItem({}));
+    dispatch(setSuccess(true));
+  };
+
+  const handleClickClose = () => {
+    setBodyScroll();
+    dispatch(setActiveItem({}));
+  };
+
+  return (
+    <div className="modal-add-to-cart" onClick={handleClickClose}>
+      <div className="modal-add-to-cart__content" onClick={(evt) => evt.stopPropagation()}>
+        <button className="modal-add-to-cart__close" onClick={handleClickClose} aria-label="Кнопка Закрыть">
+          <CloseButton />
+        </button>
+        <p className="modal-add-to-cart__title">Добавить товар в корзину</p>
+        <div className="modal-add-to-cart__wrapper">
+          <div className="modal-add-to-cart__item">
+            <div className="modal-add-to-cart__picture">
+              <img
+                src={`./assets/images/${smallImage}`}
+                srcSet={`./assets/images/${bigImage}`}
+                alt={title}
+                width="56"
+                height="128"
+              >
+              </img>
+            </div>
+            <div className="modal-add-to-cart__description">
+              <h4>Гитара {title}</h4>
+              <p className="modal-add-to-cart__text">Артикул: {article}</p>
+              <p className="modal-add-to-cart__text">{GUITAR_TYPE[type]}, {strings} струнная</p>
+              <p className="modal-add-to-cart__price">Цена: {price} &#x20bd;</p>
+            </div>
+          </div>
+          <div>
+            <button className="modal-add-to-cart__add-button" onClick={handleClickAddButton}>Добавить в корзину</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ModalAddToCart;
