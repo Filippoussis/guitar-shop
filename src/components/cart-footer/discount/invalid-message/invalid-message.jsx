2de36dd48d48aca5c3dffa4934ae2885f430d772
Andrey Filippov <androsphilippos@gmail.com>
2021-12-12 11:07:22 +0500

    создал компонент сообщения о невалидном промокоде

---
+++ src/components/cart-footer/discount/invalid-message/invalid-message.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './invalid-message.scss';
+
+function InvalidMessage() {
+  return (
+    <span className="invalid-message">Промокод не действителен</span>
+  );
+}
+
+export default InvalidMessage;
