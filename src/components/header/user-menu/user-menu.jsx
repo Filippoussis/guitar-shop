f415427cb88f2c62cb0474e358c29dde6129fa1e
Andrey Filippov <androsphilippos@gmail.com>
2021-12-12 11:16:20 +0500

    рефакт user-menu

--- src/components/header/user-menu/user-menu.jsx
+++ src/components/header/user-menu/user-menu.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {useSelector} from 'react-redux';
 
 import {ReactComponent as MapIcon} from '../../../images/icon/map.svg';
 import {ReactComponent as SearchIcon} from '../../../images/icon/search.svg';
 import {ReactComponent as CartIcon} from '../../../images/icon/cart.svg';
 
+import {selectIsCartEmpty, selectCartItemCount} from '../../../store/slices/cart';
+
 import './user-menu.scss';
 
 function UserMenu() {
+
+  const isCartEmpty = useSelector(selectIsCartEmpty);
+  const cartItemCount = useSelector(selectCartItemCount);
+
   return (
     <div className="user-menu">
       <Link to='/map' className="user-menu__item">
@@ -16,8 +23,9 @@ function UserMenu() {
       <Link to='/search' className="user-menu__item">
         <SearchIcon className="user-menu__link" />
       </Link>
-      <Link to='/cart' className="user-menu__item">
+      <Link to='/cart' className="user-menu__item user-menu__item--cart">
         <CartIcon className="user-menu__link" />
+        {!isCartEmpty && <span className="user-menu__cart-item-count">{cartItemCount}</span>}
       </Link>
     </div>
   );
