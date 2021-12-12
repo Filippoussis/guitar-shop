024466db307679905e24c7faf1588f49173108fe
Andrey Filippov <androsphilippos@gmail.com>
2021-12-12 11:12:19 +0500

    создал компонент пагинации

---
+++ src/components/products/pagination/pagination.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import {useSelector, useDispatch} from 'react-redux';
+import PropTypes from 'prop-types';
+
+import PageItem from './page-item/page-item';
+
+import {selectActivePage, setActivePage} from '../../../store/slices/guitars';
+
+import './pagination.scss';
+
+function Pagination ({pagesCount, paginationTemplate}) {
+
+  const dispatch = useDispatch();
+  const activePage = useSelector(selectActivePage);
+
+  const items = paginationTemplate.map((item, index) => <li key={`pag-${index + 1}`} className="pagination__item"><PageItem item={item}/></li>)
+
+  return (
+    <div className="pagination">
+      {activePage !== 1 && <button className="pagination__button pagination__button--back" onClick={() => dispatch(setActivePage(activePage - 1))}>Назад</button>}
+      <ul className="pagination__list">{items}</ul>
+      {activePage !== pagesCount && <button className="pagination__button pagination__button--forward" onClick={() => dispatch(setActivePage(activePage + 1))}>Далее</button>}
+    </div>
+  );
+}
+
+Pagination.propTypes = {
+  paginationTemplate: PropTypes.array.isRequired,
+};
+
+export default Pagination;
